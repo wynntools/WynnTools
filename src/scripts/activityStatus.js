f2da148aa9bd6d14d32e58fb87d984b026e4aa96
Kath <55346310+Kathund@users.noreply.github.com>
2023-07-28 10:04:14 +0800

    Activity Status

---
+++ src/scripts/activityStatus.js
@@ -0,0 +1,29 @@
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const { scriptMessage } = require('../logger.js');
+const { ActivityType } = require('discord.js');
+const path = require('path');
+const fs = require('fs');
+
+const commands = [];
+fs.readdirSync(path.resolve(__dirname, '../commands/general')).forEach((file) => {
+  if (!file.endsWith('.js')) return;
+  commands.push(file);
+});
+
+while (true) {
+  scriptMessage('Changing activity status - To Servers');
+  client.user.setPresence({
+    activities: [{ name: `to ${client.guilds.cache.size} servers!`, type: ActivityType.Listening }],
+  });
+  await delay(5 * 60_000); // 60 000 milliseconds = 1 minute
+  scriptMessage('Changing activity status - To Commands');
+  client.user.setPresence({
+    activities: [{ name: `to ${commands.length} commands!`, type: ActivityType.Listening }],
+  });
+  await delay(5 * 60_000); // 60 000 milliseconds = 1 minute
+  scriptMessage('Changing activity status - To Ping');
+  client.user.setPresence({
+    activities: [{ name: `to ${client.ws.ping}ms of ping!`, type: ActivityType.Listening }],
+  });
+  await delay(5 * 60_000); // 60 000 milliseconds = 1 minute
+}
