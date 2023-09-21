72ffaed2ac7cfa88cc07bc073e4f43e2af62923c
Kath <55346310+Kathund@users.noreply.github.com>
2023-09-21 15:01:14 +0800

    Quest To Command

--- src/commands/dev/quests.js
+++ src/commands/dev/quests.js
@@ -47,6 +47,30 @@ module.exports = {
         )
         .addBooleanOption((option) => option.setName('normal').setDescription('Hide Normal Quests').setRequired(false))
         .addBooleanOption((option) => option.setName('event').setDescription('Hide Event Quests').setRequired(false))
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('to')
+        .setDescription('Shows the best quests to get to a certain level')
+        .addStringOption((option) =>
+          option.setName('username').setDescription('The username to check').setRequired(true)
+        )
+        .addIntegerOption((option) =>
+          option
+            .setName('level')
+            .setDescription('The level to get to')
+            .setRequired(false)
+            .setMaxValue(106)
+            .setMinValue(1)
+        )
+        .addBooleanOption((option) =>
+          option.setName('dungeon').setDescription('Hide Dungeon Quests').setRequired(false)
+        )
+        .addBooleanOption((option) =>
+          option.setName('special').setDescription('Hide Special Quests').setRequired(false)
+        )
+        .addBooleanOption((option) => option.setName('normal').setDescription('Hide Normal Quests').setRequired(false))
+        .addBooleanOption((option) => option.setName('event').setDescription('Hide Event Quests').setRequired(false))
     ),
 
   async execute(interaction) {
@@ -55,8 +79,8 @@ module.exports = {
         throw new Error('No Perms');
       }
       const subcommand = await interaction.options.getSubcommand();
+      const quests = JSON.parse(fs.readFileSync('data/quests.json'));
       if (subcommand === 'list') {
-        const quests = JSON.parse(fs.readFileSync('data/quests.json'));
         const sort = interaction.options.getString('sort') || 'name';
         let sortedQuests = quests.sort((a, b) => a.name.localeCompare(b.name));
         if (sort === 'xp') {
@@ -115,7 +139,7 @@ module.exports = {
         if (completedFilter) {
           const stats = await getStats(username);
           var currentProfileStats = stats.data.characters[await getHighestProfile(stats.data.characters)];
-          var completedQuests = currentProfileStats.quests;
+          const completedQuests = currentProfileStats.quests;
           filteredQuests.forEach((quest) => {
             if (completedQuests.includes(quest.name)) {
               quest.completed = true;
@@ -136,7 +160,7 @@ module.exports = {
                 completedFilter ? config.other.emojis.yes : config.other.emojis.no
               }\nSort: ${sort}`
             )
-            .setFields(
+            .addFields(
               {
                 name: 'Name',
                 value: currentQuest.name,
@@ -246,7 +270,7 @@ module.exports = {
                       completedFilter ? config.other.emojis.yes : config.other.emojis.no
                     }\nSort: ${sort}`
                   )
-                  .setFields(
+                  .addFields(
                     {
                       name: 'Name',
                       value: currentQuest.name,
@@ -331,7 +355,7 @@ module.exports = {
                       completedFilter ? config.other.emojis.yes : config.other.emojis.no
                     }\nSort: ${sort}`
                   )
-                  .setFields(
+                  .addFields(
                     {
                       name: 'Name',
                       value: currentQuest.name,
@@ -419,7 +443,7 @@ module.exports = {
                 completedFilter ? config.other.emojis.yes : config.other.emojis.no
               }\nSort: ${sort}`
             )
-            .setFields(
+            .addFields(
               {
                 name: 'Name',
                 value: currentQuest.name,
@@ -524,7 +548,7 @@ module.exports = {
                       completedFilter ? config.other.emojis.yes : config.other.emojis.no
                     }\nSort: ${sort}`
                   )
-                  .setFields(
+                  .addFields(
                     {
                       name: 'Name',
                       value: currentQuest.name,
@@ -609,7 +633,7 @@ module.exports = {
                       completedFilter ? config.other.emojis.yes : config.other.emojis.no
                     }\nSort: ${sort}`
                   )
-                  .setFields(
+                  .addFields(
                     {
                       name: 'Name',
                       value: currentQuest.name,
@@ -684,6 +708,339 @@ module.exports = {
             errorMessage(`Error ID: ${errorIdButtons}`);
             errorMessage(error);
           }
+        }
+      } else if (subcommand === 'to') {
+        const username = interaction.options.getString('username') || null;
+        if (username == null) {
+          throw new Error('NO_ERROR_ID_You need to provide a username');
+        }
+        const stats = await getStats(username);
+        const currentProfileStats = stats.data.characters[await getHighestProfile(stats.data.characters)];
+
+        const level = interaction.options.getInteger('level') || currentProfileStats.professions.combat.level + 1;
+        const dungeonFilter = interaction.options.getBoolean('dungeon') || false;
+        const specialFilter = interaction.options.getBoolean('special') || false;
+        const normalFilter = interaction.options.getBoolean('normal') || false;
+        const eventFilter = interaction.options.getBoolean('event') || false;
+
+        var sortedQuests = quests.sort((a, b) => b.xp - a.xp);
+        const completedQuests = currentProfileStats.quests;
+        sortedQuests.forEach((quest) => {
+          if (completedQuests.includes(quest.name)) {
+            quest.completed = true;
+          } else {
+            quest.completed = false;
+          }
+        });
+        let filteredQuests = sortedQuests
+          .filter((quest) => quest.combatMinLvl > currentProfileStats.professions.combat.level)
+          .filter((quest) => quest.completed === false);
+        if (
+          (dungeonFilter && specialFilter) ||
+          (dungeonFilter && normalFilter) ||
+          (dungeonFilter && eventFilter) ||
+          (specialFilter && normalFilter) ||
+          (specialFilter && eventFilter) ||
+          (normalFilter && eventFilter)
+        ) {
+          throw new Error('NO_ERROR_ID_Please only select one filter at a time (Completed is not counted)');
+        }
+        if (filteredQuests.length === 0) {
+          throw new Error('NO_ERROR_ID_There are no quests that you can do to get to that level');
+        }
+        if (dungeonFilter) {
+          filteredQuests = sortedQuests.filter((quest) => quest.type !== 'Dungeon');
+        }
+        if (specialFilter) {
+          filteredQuests = sortedQuests.filter((quest) => quest.type !== 'Special');
+        }
+        if (normalFilter) {
+          filteredQuests = sortedQuests.filter((quest) => quest.type !== 'Normal');
+        }
+        if (eventFilter) {
+          filteredQuests = sortedQuests.filter((quest) => quest.type !== 'Event');
+        }
+
+        if (filteredQuests.length === 0) {
+          throw new Error(
+            'NO_ERROR_ID_There are no quests that you can do to get to that level with the filters you have selected'
+          );
+        }
+
+        let num = 0;
+        let currentQuest = filteredQuests[num];
+
+        var responseEmbed = new EmbedBuilder()
+          .setColor(config.other.colors.green.hex)
+          .setTitle(`Best Quests to get to level ${level}`)
+          .setDescription(
+            `**Quest Filters:**\nDungeon Filter: ${
+              dungeonFilter ? config.other.emojis.yes : config.other.emojis.no
+            }\nSpecial Filter: ${
+              specialFilter ? config.other.emojis.yes : config.other.emojis.no
+            }\nNormal Filter: ${normalFilter}\nEvent Filter: ${eventFilter}\n\n${num}/${filteredQuests.length}`
+          )
+          .addFields(
+            {
+              name: 'Name',
+              value: currentQuest.name,
+              inline: true,
+            },
+            {
+              name: 'Xp',
+              value: currentQuest.xp,
+              inline: true,
+            },
+            {
+              name: 'Emeralds',
+              value: currentQuest.emeralds,
+              inline: true,
+            },
+            {
+              name: 'Wiki Url',
+              value: currentQuest.wikiUrl,
+              inline: true,
+            },
+            {
+              name: 'Combat Min Lvl',
+              value: currentQuest.combatMinLvl === null ? 'None' : currentQuest.combatMinLvl,
+              inline: true,
+            },
+            {
+              name: 'Mining Min Lvl',
+              value: currentQuest.miningMinLvl === null ? 'None' : currentQuest.miningMinLvl,
+              inline: true,
+            },
+            {
+              name: 'Wood Cutting Min Lvl',
+              value: currentQuest.woodCuttingMinLvl === null ? 'None' : currentQuest.woodCuttingMinLvl,
+              inline: true,
+            },
+            {
+              name: 'Farming Min Lvl',
+              value: currentQuest.farmingMinLvl === null ? 'None' : currentQuest.farmingMinLvl,
+              inline: true,
+            },
+            {
+              name: 'Fishing Min Lvl',
+              value: currentQuest.fishingMinLvl === null ? 'None' : currentQuest.fishingMinLvl,
+              inline: true,
+            },
+            {
+              name: 'Type',
+              value: currentQuest.type,
+              inline: true,
+            },
+            {
+              name: 'Length',
+              value: currentQuest.length,
+              inline: true,
+            },
+            {
+              name: 'Main Province',
+              value: currentQuest.mainProvince,
+              inline: true,
+            }
+          )
+          .setTimestamp()
+          .setFooter({
+            text: `by @kathund | ${config.discord.supportInvite} for support`,
+            iconURL: config.other.logo,
+          });
+
+        const leftButton = new ButtonBuilder()
+          .setEmoji(config.other.emojis.left)
+          .setStyle(ButtonStyle.Secondary)
+          .setCustomId('leftButtonQuests');
+
+        const rightButton = new ButtonBuilder()
+          .setEmoji(config.other.emojis.right)
+          .setStyle(ButtonStyle.Secondary)
+          .setCustomId('rightButtonQuests');
+
+        const row = new ActionRowBuilder().addComponents(leftButton, rightButton);
+
+        const message = await interaction.reply({ embeds: [responseEmbed], components: [row] });
+        const collectorFilter = (i) => i.user.id === interaction.user.id;
+        try {
+          while (true) {
+            var confirmationQuestsTo = await message.awaitMessageComponent({
+              time: config.discord.buttonTimeout * 1000,
+              filter: collectorFilter,
+            });
+
+            if (confirmationQuestsTo.customId === 'leftButtonQuests') {
+              num = num - 1;
+              if (num <= 0) num = filteredQuests.length - 1;
+              currentQuest = filteredQuests[num];
+
+              const questEmbed = new EmbedBuilder()
+                .setColor(config.other.colors.green.hex)
+                .setTitle(`Best Quests to get to level ${level}`)
+                .setDescription(
+                  `**Quest Filters:**\nDungeon Filter: ${
+                    dungeonFilter ? config.other.emojis.yes : config.other.emojis.no
+                  }\nSpecial Filter: ${
+                    specialFilter ? config.other.emojis.yes : config.other.emojis.no
+                  }\nNormal Filter: ${normalFilter}\nEvent Filter: ${eventFilter}\n\n${num}/${filteredQuests.length}`
+                )
+                .addFields(
+                  {
+                    name: 'Name',
+                    value: currentQuest.name,
+                    inline: true,
+                  },
+                  {
+                    name: 'Xp',
+                    value: currentQuest.xp,
+                    inline: true,
+                  },
+                  {
+                    name: 'Emeralds',
+                    value: currentQuest.emeralds,
+                    inline: true,
+                  },
+                  {
+                    name: 'Wiki Url',
+                    value: currentQuest.wikiUrl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Combat Min Lvl',
+                    value: currentQuest.combatMinLvl === null ? 'None' : currentQuest.combatMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Mining Min Lvl',
+                    value: currentQuest.miningMinLvl === null ? 'None' : currentQuest.miningMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Wood Cutting Min Lvl',
+                    value: currentQuest.woodCuttingMinLvl === null ? 'None' : currentQuest.woodCuttingMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Farming Min Lvl',
+                    value: currentQuest.farmingMinLvl === null ? 'None' : currentQuest.farmingMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Fishing Min Lvl',
+                    value: currentQuest.fishingMinLvl === null ? 'None' : currentQuest.fishingMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Type',
+                    value: currentQuest.type,
+                    inline: true,
+                  },
+                  {
+                    name: 'Length',
+                    value: currentQuest.length,
+                    inline: true,
+                  },
+                  {
+                    name: 'Main Province',
+                    value: currentQuest.mainProvince,
+                    inline: true,
+                  }
+                )
+                .setTimestamp()
+                .setFooter({
+                  text: `by @kathund | ${config.discord.supportInvite} for support`,
+                  iconURL: config.other.logo,
+                });
+              await confirmationQuestsTo.update({ embeds: [questEmbed], components: [row] });
+            } else if (confirmationQuestsTo.customId === 'rightButtonQuests') {
+              num = num + 1;
+              if (num <= 0) num = filteredQuests.length - 1;
+              currentQuest = filteredQuests[num];
+
+              const questEmbed = new EmbedBuilder()
+                .setColor(config.other.colors.green.hex)
+                .setTitle(`Best Quests to get to level ${level}`)
+                .setDescription(
+                  `**Quest Filters:**\nDungeon Filter: ${
+                    dungeonFilter ? config.other.emojis.yes : config.other.emojis.no
+                  }\nSpecial Filter: ${
+                    specialFilter ? config.other.emojis.yes : config.other.emojis.no
+                  }\nNormal Filter: ${normalFilter}\nEvent Filter: ${eventFilter}\n\n${num}/${filteredQuests.length}`
+                )
+                .addFields(
+                  {
+                    name: 'Name',
+                    value: currentQuest.name,
+                    inline: true,
+                  },
+                  {
+                    name: 'Xp',
+                    value: currentQuest.xp,
+                    inline: true,
+                  },
+                  {
+                    name: 'Emeralds',
+                    value: currentQuest.emeralds,
+                    inline: true,
+                  },
+                  {
+                    name: 'Wiki Url',
+                    value: currentQuest.wikiUrl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Combat Min Lvl',
+                    value: currentQuest.combatMinLvl === null ? 'None' : currentQuest.combatMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Mining Min Lvl',
+                    value: currentQuest.miningMinLvl === null ? 'None' : currentQuest.miningMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Wood Cutting Min Lvl',
+                    value: currentQuest.woodCuttingMinLvl === null ? 'None' : currentQuest.woodCuttingMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Farming Min Lvl',
+                    value: currentQuest.farmingMinLvl === null ? 'None' : currentQuest.farmingMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Fishing Min Lvl',
+                    value: currentQuest.fishingMinLvl === null ? 'None' : currentQuest.fishingMinLvl,
+                    inline: true,
+                  },
+                  {
+                    name: 'Type',
+                    value: currentQuest.type,
+                    inline: true,
+                  },
+                  {
+                    name: 'Length',
+                    value: currentQuest.length,
+                    inline: true,
+                  },
+                  {
+                    name: 'Main Province',
+                    value: currentQuest.mainProvince,
+                    inline: true,
+                  }
+                )
+                .setTimestamp()
+                .setFooter({
+                  text: `by @kathund | ${config.discord.supportInvite} for support`,
+                  iconURL: config.other.logo,
+                });
+              await confirmationQuestsTo.update({ embeds: [questEmbed], components: [row] });
+            }
+          }
+        } catch (error) {
+          var errorIdBQuestsToButtons = generateID(config.other.errorIdLength);
+          errorMessage(`Error ID: ${errorIdBQuestsToButtons}`);
+          errorMessage(error);
         }
       }
     } catch (error) {
